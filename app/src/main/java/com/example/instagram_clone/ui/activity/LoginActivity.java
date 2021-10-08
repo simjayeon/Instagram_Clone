1b6c9405ecd10f54e659be072bc35a438415bf03
simjayeon98@gmail.com <simjayeon98@gmail.com>
2021-10-08 23:11:46 +0900

    구글 로그인 기능 추가

--- app/src/main/java/com/example/instagram_clone/ui/activity/LoginActivity.java
+++ app/src/main/java/com/example/instagram_clone/ui/activity/LoginActivity.java
@@ -1,6 +1,7 @@
-package com.example.instagram_clone.activity;
+package com.example.instagram_clone.ui.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,7 +12,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.instagram_clone.R;
-import com.example.instagram_clone.data.UserInfo;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -21,9 +26,10 @@ import com.google.firebase.auth.FirebaseUser;
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth; // FirebaseAuth 객체의 공유 인스턴스
+    private GoogleSignInClient googleSignInClient; // 구글
+    private GoogleSignInOptions gso;
+
     EditText edit_email, edit_password;
-    //UserInfo userInfo;
-    //SignUpActivity signUpActivity;
 
 
     @Override
@@ -35,6 +41,16 @@ public class LoginActivity extends AppCompatActivity {
         edit_password = (EditText) findViewById(R.id.edit_password);
         TextView btn_login = (TextView) findViewById(R.id.btn_login);
         TextView btn_signUp = (TextView) findViewById(R.id.btn_signup);
+        TextView btn_login_google = (TextView) findViewById(R.id.btn_google);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(LoginActivity.this, gso);
+
+
 
         btn_signUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +64,13 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 signIn();
+            }
+        });
+
+        btn_login_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleLogin();
             }
         });
 
@@ -66,7 +89,8 @@ public class LoginActivity extends AppCompatActivity {
                             moveMainPage(task.getResult().getUser()); // 뭔지 알아내기
                         }else{
                             //회원가입 실패시
-                            Toast.makeText(LoginActivity.this, "회원가입 실패", Toast.LENGTH_SHORT);
+                            //다이얼로그로 변경하기
+                            Toast.makeText(LoginActivity.this, "회원가입 실패", Toast.LENGTH_SHORT).show();
                         }
 
                     }
@@ -83,11 +107,16 @@ public class LoginActivity extends AppCompatActivity {
                             moveMainPage(task.getResult().getUser()); // 뭔지 알아내기
                         }else{
                             //회원가입 실패시
-                            Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT);
+                            Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
                         }
 
                     }
                 });
+    }
+
+    public void googleLogin(){
+        Intent intent = googleSignInClient.getSignInIntent(); //구글 클라이언트에서 로그인화면으로 이동
+        startActivityForResult(intent, 1);
     }
 
     public void moveMainPage(FirebaseUser user){
@@ -95,4 +124,49 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(new Intent(this, MainActivity.class));
         }
     }
+
+
+    //로그인 요청
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            if(task.isSuccessful()){
+                //로그인 성공일 경우
+                try{
+                    GoogleSignInAccount acct = task.getResult(ApiException.class);
+                    firebaseAuthWithGoogle(acct.getIdToken());
+
+                }catch (ApiException e){
+
+                }
+
+            }else{
+                //로그인 실패일 경우
+            }
+        }
+    }
+
+    //파이어베이스로 값 저장 (뺄까 고민중)
+    private void firebaseAuthWithGoogle(String idToken) {
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
